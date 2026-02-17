661812674529b9796e2101c5a7f7f6a89c9c7f45
natsuti <axbex1310@gmail.com>
2026-02-16 23:07:56 -0500

    primera version que sirve sin errores de logica(sin precendencia)

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 todo = input("calcula: ")
 tokens = []
-resultado = 0
+resultado = None
 buffer = ""
 # Se separa todo, la verdad es bien dificil separar los procesos, en esta caso primero la parte de la tokenizacion(lexer).
 for i in range(len(todo)):
@@ -20,37 +20,25 @@ print(f"3. buffer: {buffer}, tokens: {tokens}")
 # Si la estructura muta, el indice debe estar bajo control explicito
 # Ahora toca el parser(transforma los tokens en algo o eso creo, no importa la verdad, solo se que hace en codigo)
 # Esto es un problema
-nuevostokens = []
-i = 0
-while i < len(tokens) - 1:
-    if resultado != 0:
-        if i % 2 != 0:
-            if tokens[i] == "*":
-                resultado *= int(tokens[i + 1])
-                nuevostokens.append(resultado)
-            elif tokens[i] == "/":
-                resultado /= int(tokens[i + 1])
-                nuevostokens.append(resultado)
-    else:
-        if i % 2 != 0:
-            if tokens[i] == "*":
-                resultado = int(tokens[i - 1]) * int(tokens[i + 1])
-            elif tokens[i] == "/":
-                resultado = int(tokens[i - 1]) / int(tokens[i + 1])
-    i += 1
-i = 0
-while i < len(tokens) - 1:
-    if resultado != 0:
-        if i % 2 != 0:
+for i in range(len(tokens)):
+    if resultado:
+        if tokens[i] in "+-*/":
             if tokens[i] == "+":
                 resultado += int(tokens[i + 1])
             elif tokens[i] == "-":
                 resultado -= int(tokens[i + 1])
+            elif tokens[i] == "*":
+                resultado *= int(tokens[i + 1])
+            elif tokens[i] == "/":
+                resultado /= int(tokens[i + 1])
     else:
-        if i % 2 != 0:
+        if tokens[i] in "+-*/":
             if tokens[i] == "+":
                 resultado = int(tokens[i - 1]) + int(tokens[i + 1])
             elif tokens[i] == "-":
                 resultado = int(tokens[i - 1]) - int(tokens[i + 1])
-    i += 1
+            elif tokens[i] == "*":
+                resultado = int(tokens[i - 1]) * int(tokens[i + 1])
+            elif tokens[i] == "/":
+                resultado = int(tokens[i - 1]) / int(tokens[i + 1])
 print(resultado)
